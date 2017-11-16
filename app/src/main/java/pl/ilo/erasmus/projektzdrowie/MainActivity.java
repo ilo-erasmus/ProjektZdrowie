897c05ea7ae532614c36b36731edf96758e1d2f4
wvffle <casper@wvffle.net>
2017-11-16 12:21:22 +0100

    initial commit

---
+++ app/src/main/java/pl/ilo/erasmus/projektzdrowie/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.ilo.erasmus.projektzdrowie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
